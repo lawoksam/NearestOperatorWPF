4e4446c3bd17ee3bbb471db5767c4d4d9fb0ca66
lawoksam <lawoksam@gmail.com>
2022-04-20 20:38:00 +0200

    working final 1

--- PositionApi/Data.cs
+++ PositionApi/Data.cs
@@ -2,8 +2,8 @@
 {
     internal class Data
     {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
         public string Type { get; set; }
         public string Name { get; set; }
         public string Number { get; set; }
